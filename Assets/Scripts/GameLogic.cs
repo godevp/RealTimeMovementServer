60a9522bc777e0cddc6346adbc8731c0d7480fda
godevp <73740267+godevp@users.noreply.github.com>
2022-12-10 02:06:41 -0500

    brainfarting

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -4,14 +4,53 @@ using UnityEngine;
 
 public class GameLogic : MonoBehaviour
 {
+    const float CharacterSpeed = 0.13f;
+    float fixedDeltaTime;
+    public LinkedList<Player> listOfPlayers = new LinkedList<Player>();
     void Start()
     {
         NetworkedServerProcessing.SetGameLogic(this);
+        fixedDeltaTime = Time.fixedDeltaTime;
     }
 
     void Update()
     {
-
+       
     }
 
+    public void SetNewPlayer(int id)
+    {
+        Player newPlayer = new Player();
+        newPlayer.id = id;
+        listOfPlayers.AddLast(newPlayer);
+        NetworkedServerProcessing.SendMessageToClient(ServerToClientSignifiers.RequestForPositionAndGivingSpeed.ToString() + '|'+ fixedDeltaTime + '|' + CharacterSpeed , id);
+    }
+    public void SetPlayerPosition(float posX, float posY, int id)
+    {
+        foreach (Player player in listOfPlayers)
+        {
+            if (player.id == id)
+            {
+                player._pos = new Vector2(posX,posY);
+               foreach(Player p in listOfPlayers)
+               {
+                    if(p.id != id)
+                    {
+                        NetworkedServerProcessing.SendMessageToClient(ServerToClientSignifiers.NewClientJoined.ToString() + '|'
+                                                                      + player.id + '|' + player._pos.x + '|' + player._pos.y, p.id);
+                    }
+               }
+                break;
+            }
+        }
+    }
+}
+public class Player
+{
+    public int id;
+    public Vector2 _pos;
+    public bool pressW;
+    public bool pressS;
+    public bool pressA;
+    public bool pressD;
 }
--- Assets/Scripts/NetworkedServerProcessing.cs
+++ Assets/Scripts/NetworkedServerProcessing.cs
@@ -10,19 +10,18 @@ static public class NetworkedServerProcessing
     {
         Debug.Log("msg received = " + msg + ".  connection id = " + clientConnectionID);
 
-        string[] csv = msg.Split(',');
-        int signifier = int.Parse(csv[0]);
-
-        if (signifier == ClientToServerSignifiers.asd)
+        string[] csv = msg.Split('|');
+        int signifier = 0;
+        if (int.TryParse(csv[0], out _))
         {
-
+            signifier = int.Parse(csv[0]);
         }
-        // else if (signifier == ClientToServerSignifiers.asd)
-        // {
-
-        // }
-
-        //gameLogic.DoSomething();
+        switch (signifier)
+        {
+            case ClientToServerSignifiers.HereIsMyPosition:
+                gameLogic.SetPlayerPosition(float.Parse(csv[1]), float.Parse(csv[2]), clientConnectionID);
+                break;
+        }
     }
     static public void SendMessageToClient(string msg, int clientConnectionID)
     {
@@ -42,10 +41,20 @@ static public class NetworkedServerProcessing
     static public void ConnectionEvent(int clientConnectionID)
     {
         Debug.Log("New Connection, ID == " + clientConnectionID);
+        gameLogic.SetNewPlayer(clientConnectionID);
     }
     static public void DisconnectionEvent(int clientConnectionID)
     {
         Debug.Log("New Disconnection, ID == " + clientConnectionID);
+        foreach(Player player in gameLogic.listOfPlayers)
+        {
+            if(player.id == clientConnectionID)
+            {
+                gameLogic.listOfPlayers.Remove(player);
+                break;
+            }
+        }
+        
     }
 
     #endregion
@@ -73,12 +82,13 @@ static public class NetworkedServerProcessing
 #region Protocol Signifiers
 static public class ClientToServerSignifiers
 {
-    public const int asd = 1;
+    public const int HereIsMyPosition = 1;
 }
 
 static public class ServerToClientSignifiers
 {
-    public const int asd = 1;
+    public const int RequestForPositionAndGivingSpeed = 1;
+    public const int NewClientJoined = 2;
 }
 
 #endregion
